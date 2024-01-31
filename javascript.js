1f60203cbba0467a3ea86b9d274b30d26c032396
Armando Sampaio <armandoosampai0@gmail.com>
2024-01-31 16:42:14 -0300

    Função para fazer uma melhor de 5 Criada

--- javascript.js
+++ javascript.js
@@ -19,8 +19,35 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
+function playGame(){
+    let counter = 1;
+    let playerWins = 0;
+    let computerWins = 0;
 
 
+    while(counter <= 5){
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Digite Pedra, Papel ou Tesoura");
+        let result = playRound(playerChoice, computerChoice);
+
+        if(result === "ganhou"){
+            playerWins++;
+        }
+        alert("O computador Escolheu: " + computerChoice + "\nVocê " + result);
+        counter++;
+    }
+
+    if(playerWins > computerWins){
+        alert("Você ganhou a melhor de 5 Parabens");
+    }else if(playerWins < computerWins){
+        alert("Você Perdeu");
+    }else{
+        alert("Você Empatou")
+    }
+}
+
+
+playGame();
 
 
 
